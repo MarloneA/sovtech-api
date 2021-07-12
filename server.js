65ef3107dc7da5eac6fc4e0174c360775d3e2559
Marlone Akidiva <marlone911@gmail.com>
2021-07-12 09:55:54 +0300

    deploy server

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.use(
 
 // set port, listen for requests
 const PORT = process.env.PORT || 80;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}.`);
 });

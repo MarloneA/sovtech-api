60836f29838a47cc7f921a548276a76ae25a6635
Marlone Akidiva <marlone911@gmail.com>
2021-07-12 13:35:17 +0300

    debug server js file

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.use(
 
 // set port, listen for requests
 const PORT = process.env.PORT || 80;
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
